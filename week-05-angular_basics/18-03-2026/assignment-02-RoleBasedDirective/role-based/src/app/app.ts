cb0b54937a1ecc033b66392aa332637070737180
AkshatTs <akshat5574nkpsem@gmail.com>
2026-03-18 13:04:46 +0530

    assignment-2-18march

---
+++ week-05-angular_basics/18-03-2026/assignment-02-RoleBasedDirective/role-based/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RoleDirective } from './role';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RoleDirective],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {}
